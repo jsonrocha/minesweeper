50d57a536c21f4201f3e8701dbe273c49dd087f0
Rocha <jrocha@proforma.com>
2018-08-23 19:37:56 -0400

    ExplorerMode, working on CSS/polishing still.

--- minesweeper/src/Minesweeper.js
+++ minesweeper/src/Minesweeper.js
@@ -8,17 +8,22 @@ class Minesweeper extends Component {
         this.state = {
             game: {
                 board: []
-            }
+            },
+            message:''
         }
     }
 
     componentDidMount() {
         // create the board
         // create board 
-        fetch(BASE_URL + "games", {
+        fetch(`${BASE_URL}games`,  {
             method: "POST",
-            body: JSON.stringify({ difficulty: 0 })
-        }).then(resp => resp.json())
+            body: JSON.stringify({ difficulty: 0 }),
+            headers: {
+                'Content-Type':'application/json'
+            }
+        })
+        .then(resp => resp.json())
             .then(newGame => {
                 console.log("game", newGame);
                 this.setState({
@@ -27,27 +32,86 @@ class Minesweeper extends Component {
             })
     }
 
+    displayGameResult () {
+        if (this.state.game.state === "lost") {
+            //display lost message
+            this.setState({
+            message: "You lost!"
+            })
+        } else if (this.state.game.state === "win") {
+            this.setState({
+                message:"Oh yeah! You won!"
+            })
+        } else {
+            this.setState({
+                message: "still sweepin..."
+            })
+        }
+    }
+
+    handleCellClick = (row, col) => {
+        if (!(this.state.game.state === "lost" || this.state.game.state === "won")) {
+            fetch(`${BASE_URL}games/${this.state.game.id}/check`,  {
+                method: "POST",
+                body: JSON.stringify({ row:row, col:col }),
+                headers: {
+                    'Content-Type':'application/json'
+                }
+            })
+            .then(resp => resp.json())
+                .then(newGameState => {
+                    console.log(newGameState);
+                        this.setState({
+                            game: newGameState,
+                        })
+                        this.displayGameResult()
+                })
+        }
+
+    }
+
+    handleFlaggedCell = (event, row, col) => {
+        console.log(event)
+        event.preventDefault( )
+        console.log('flagged', row,col)
+        fetch(`${BASE_URL}games/${this.state.game.id}/flag`,  {
+            method: "POST",
+            body: JSON.stringify({ row:row, col:col }),
+            headers: {
+                'Content-Type':'application/json'
+            }
+        })
+        .then(resp => resp.json())
+            .then(newGameState => {
+                console.log("game", newGameState);
+                this.setState({
+                    game: newGameState
+                })
+            })
+
+    }
 
     render() {
         return (
             <div>
-                currently playing   {this.state.game.id}
-
-                {this.state.game.board.map((row, i) => {
-                    console.log("row", row, i)
-                    return (
-                        <div className="squarecontainer">
-                            {row.map((col, j) => {
-                                return <span className="square">
-                                    {this.state.game.board[i][j]}
-                                    {/* is at
-                                {`${i}, ${j}`} */}
-                                </span>
-                            })}
-                        </div>
-                    )
-                    return
-                })}
+                  <h1>{this.state.message}</h1>
+               <table>
+                   <tbody>
+                       {this.state.game.board.map((row, i) => {
+                         return <tr key={i}>{
+                             row.map((col, j) => {
+                                return <td 
+                                className="cell" 
+                                key={j}
+                                onClick={() => this.handleCellClick(i,j)}
+                                onContextMenu={(event) => this.handleFlaggedCell(event, i,j)}
+                                >{this.state.game.board[i][j]}</td>
+                             })
+                         }</tr>
+                       })}
+                   </tbody>
+               </table>
+            
             </div>
         );
     }
